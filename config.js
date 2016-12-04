f466a218d740fefe28546ee8bfa099cd41901d46
george <george@macbook-2.local>
2016-12-04 13:59:03 -0600

    change scripts

--- config.js
+++
@@ -1,3 +0,0 @@
-module.export = {
-  name: 'webpage'
-}
--- package-scripts.js
+++ package-scripts.js
@@ -2,6 +2,7 @@
 const name = require('./package.json').name
 const registry = 'registry.jorgeadolfo.com'
 const tag = 'latest'
+const version = require('./package.json').version
 
 module.exports = {
   scripts: {
@@ -9,11 +10,19 @@ module.exports = {
     test: 'mocha',
     lint: 'standard --verbose | snazzy',
     docker: {
-      build: `docker build -f docker/Dockerfile -t ${registry}/${name}:${tag} . `,
-      push: `docker push ${registry}/${name}:${tag}`,
+      build: {
+        all: 'nps d.b.l && nps d.b.v',
+        latest: `docker build -f docker/Dockerfile -t ${registry}/${name}:latest . `,
+        version: `docker build -f docker/Dockerfile -t ${registry}/${name}:${version} . `
+      },
+      push: {
+        all: 'nps d.p.l && nps d.p.v',
+        latest: `docker push ${registry}/${name}:${tag}`,
+        version: `docker push ${registry}/${name}:${version}`
+      },
       run: `docker run -it --rm -p 3000:3003 --name test-${name} ${registry}/${name}:${tag}`
     },
     deploy: "ansible-playbook -v deploy/update.yml",
-    full: "nps d.b && nps d.p && nps de"
+    full: "nps d.b.a && nps d.p.a && nps de"
   }
 };
